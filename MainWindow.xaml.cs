7f68e09baa8e7abc4b16f76996e7cca3a755954b
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-03-26 14:16:31 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,67 +1,24 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace databaze
+namespace IT2A_database
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<Movie> Movies { get; set; } = new ObservableCollection<Movie>();
-
         public MainWindow()
         {
             InitializeComponent();
-
-            Movies.Add(new Movie { Name = "Inception", Genre = "Sci-Fi", Length = 148, Rating = 90 });
-            Movies.Add(new Movie { Name = "Titanic", Genre = "Romantický", Length = 195, Rating = 85 });
-            Movies.Add(new Movie { Name = "Avengers", Genre = "Akční", Length = 143, Rating = 88 });
-
-            DataContext = this;
-        }
-
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            Movies.Add(new Movie
-            {
-                Name = tbName.Text,
-                Genre = tbGenre.Text,
-                Length = int.Parse(tbLength.Text),
-                Rating = int.Parse(tbRating.Text)
-            });
-        }
-
-        private void Load_Click(object sender, RoutedEventArgs e)
-        {
-            if (dgFilmy.SelectedItem is Movie m)
-            {
-                tbName.Text = m.Name;
-                tbGenre.Text = m.Genre;
-                tbLength.Text = m.Length.ToString();
-                tbRating.Text = m.Rating.ToString();
-            }
-        }
-
-        private void Update_Click(object sender, RoutedEventArgs e)
-        {
-            if (dgFilmy.SelectedItem is Movie m)
-            {
-                m.Name = tbName.Text;
-                m.Genre = tbGenre.Text;
-                m.Length = int.Parse(tbLength.Text);
-                m.Rating = int.Parse(tbRating.Text);
-                dgFilmy.Items.Refresh();
-            }
-        }
-
-        private void Delete_Click(object sender, RoutedEventArgs e)
-        {
-            if (dgFilmy.SelectedItem is Movie m)
-                Movies.Remove(m);
-        }
-
-        private void dgFilmy_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
--- Movie.cs
+++ Movie.cs
@@ -1,7 +1,16 @@
-﻿public class Movie
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT2A_database
 {
-    public string Name { get; set; }
-    public string Genre { get; set; }
-    public int Length { get; set; }
-    public int Rating { get; set; }
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Length { get; set; }
+        public int Rating { get; set; }
+    }
 }
